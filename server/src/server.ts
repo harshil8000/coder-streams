e7f729db297bf95126a43d873e24127751e3d976
harshil800 <baradharshilbhai@gmail.com>
2024-08-07 22:10:04 +0530

    ss

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,11 @@ const app = express()
 app.use(express.json())
 
 app.use(cors())
-
+app.use(cors({
+	origin: 'https://coder-streams-client.vercel.app',
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	credentials: true
+  }));
 app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 
 const server = http.createServer(app)
