9383aa548376ef62eeb4f203aabc9894d08043e4
harshil800 <baradharshilbhai@gmail.com>
2024-08-07 22:41:32 +0530

    cc

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,11 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 
 app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 
@@ -22,7 +26,8 @@ const io = new Server(server, {
 	cors: {
 		origin: "*",
 	},
-	
+	maxHttpBufferSize: 1e8,
+	pingTimeout: 60000,
 })
 
 let userSocketMap: User[] = []
